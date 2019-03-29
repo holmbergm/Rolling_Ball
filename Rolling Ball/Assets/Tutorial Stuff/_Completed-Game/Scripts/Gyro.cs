d2dbb2c3b710b9743c26e474fb85cd52f6740e6b
THEPOGTOP\Pogal <459793@chrome.sussexdowns.ac.uk>
2019-03-29 11:51:34 +0000

    More testing

--- Rolling Ball/Assets/Tutorial Stuff/_Completed-Game/Scripts/Gyro.cs
+++ Rolling Ball/Assets/Tutorial Stuff/_Completed-Game/Scripts/Gyro.cs
@@ -1,46 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
 public class Gyro : MonoBehaviour
 {
-	private bool gyroEnabled;
-	private Gyroscope gyro;
-	public Text GryroDisabled;
+    private bool gyroEnabled;
+    private Gyroscope gyro;
 
-	private void Start()
-	{
-		gyroEnabled = EnableGyro();
-		Screen.orientation = ScreenOrientation.LandscapeLeft;
-		GryroDisabled.gameObject.SetActive(false);
-	}
-	
-	private bool EnableGyro()
-	{
-		if (SystemInfo.supportsGyroscope)
-		{
-			gyro = Input.gyro;
-			gyro.enabled = true;
-			return true;
-		}
-		return false;
-	}
+    private void Start()
+    {
+        gyroEnabled = EnableGyro();
+    }
 
-	public void Update()
-	{
-		if (gyroEnabled == true)
-		{
-			float x = -gyro.rotationRate.x;
-			float y = -gyro.rotationRate.y;
-			float z = -gyro.rotationRate.z;
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+        return false;
+    }
 
-			transform.eulerAngles = new Vector3(transform.eulerAngles.x + x, transform.eulerAngles.y + z, transform.eulerAngles.z + y);
+    public void Update()
+    {
+        if(gyroEnabled == true)
+        {
+            float x = gyro.rotationRate.x;
+            float y = gyro.rotationRate.y;
+            float z = gyro.rotationRate.z;
 
-		}
-		else
-		{
-			GryroDisabled.gameObject.SetActive(true);
-		}
-	}
+            transform.eulerAngles = new Vector3(x, y, z);
+
+        }
+    }
 }
